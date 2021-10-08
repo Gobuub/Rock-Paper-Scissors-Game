ff5956c8e2ef2ec18ca2be5e25c364bb75acd280
Gobuub <botica1982@msn.com>
2021-10-08 19:27:59 +0200

    Add Main Title

--- main.py
+++ main.py
@@ -9,14 +9,13 @@ import utilities.logic as lg
 
 # With this command iniciate the program
 if __name__ == "__main__":
+    print("****   ****    ****")
+    print("*   *  *   *  *    *")
+    print("****   ****    *")
+    print("*   *  *         *  ")
+    print("*   *  *      *    * ")
+    print("*   *  *       ****")
     # With this sentence set the choice value taking it by terminal argv
-    if sys.argv[1] == "":
-        print("Escoje una opción.")
-        print("pi = Piedra")
-        print("pa = Papel")
-        print("ti = Tijeras")
-        print(str(input("Introduce tu opción:")))
-    else:
-        lg.choice = sys.argv[1]
+    lg.choice = sys.argv[1]
     # Execute the game with the choice value
     lg.rps_game(lg.choice)
